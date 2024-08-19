d647119250df7f4a88a6070c246257c2384c6f2b
Alex Foster <xrk4np@virginia.edu>
2024-08-19 14:39:15 -0400

    aesthetic improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16">
         <div className="flex flex-col items-center justify-center space-y-2">
           <h1 className="max-w-screen-md text-center text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             analyse your instagram vibe
           </h1>
-          <h2 className="text-muted-foreground">
+          <h2 className="text-muted-foreground text-center">
             Upload screenshots of your Instagram profile below to get started
           </h2>
         </div>
